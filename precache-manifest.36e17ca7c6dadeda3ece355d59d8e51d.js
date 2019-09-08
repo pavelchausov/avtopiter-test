274bba6e745d12409c16cc4aad57b0fbeb748bff
pavelchausov <pavel.chausov@gmail.com>
2019-09-09 01:10:40 +0300

    Updates

---
+++ precache-manifest.36e17ca7c6dadeda3ece355d59d8e51d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ac62b591b61050891063b81ff12618",
+    "url": "/avtopiter/index.html"
+  },
+  {
+    "revision": "d63ec0d6b3a0f6f0daaf",
+    "url": "/avtopiter/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a032fd2f332aba3aa55b",
+    "url": "/avtopiter/static/css/main.91d7d250.chunk.css"
+  },
+  {
+    "revision": "d63ec0d6b3a0f6f0daaf",
+    "url": "/avtopiter/static/js/2.c628ed7e.chunk.js"
+  },
+  {
+    "revision": "a032fd2f332aba3aa55b",
+    "url": "/avtopiter/static/js/main.ab2d69d6.chunk.js"
+  },
+  {
+    "revision": "061eb4c21eba9647bbea",
+    "url": "/avtopiter/static/js/runtime~main.9d5aa424.js"
+  },
+  {
+    "revision": "8beb4b2d39596200d51099b52290cace",
+    "url": "/avtopiter/static/media/checkMark.8beb4b2d.svg"
+  },
+  {
+    "revision": "961f03f79808faf8e86f2f7c04a05d45",
+    "url": "/avtopiter/static/media/plus.961f03f7.svg"
+  },
+  {
+    "revision": "c1ae67a7f017170e3f2733b81a18f635",
+    "url": "/avtopiter/static/media/trashButton.c1ae67a7.svg"
+  }
+]);
